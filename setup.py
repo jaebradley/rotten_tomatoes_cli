b13641cc5374e2488854838a318d9c0a360511cb
jaebradley <jae.b.bradley@gmail.com>
2017-05-29 21:33:28 -0400

    formatting

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rotten_tomatoes_cli',
-    version='0.1',
+    name="rotten_tomatoes_cli",
+    version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
-        'rotten_tomatoes_client==0.0.2',
-        'terminaltables',
-        'termcolor'
+        "Click==6.7",
+        "rotten_tomatoes_client==0.0.3",
+        "terminaltables==3.1.0",
+        "termcolor==1.1.0"
     ],
     entry_points={
-        'console_scripts': [
-            'rotten= scripts.rotten:rotten'
+        "console_scripts": [
+            "rotten= scripts.rotten:rotten"
         ],
     }
 )
