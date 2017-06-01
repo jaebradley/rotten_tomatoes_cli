bad030220628992f6ced1236e7d505dc533067e5
jaebradley <jae.b.bradley@gmail.com>
2017-05-31 21:17:30 -0400

    searcher test

---
+++ data/test_rottenTomatoesSearcher.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+from mock import Mock, patch
+
+from data.services import RottenTomatoesSearcher
+from data import SearchResult
+
+
+class TestRottenTomatoesSearcher(TestCase):
+    searcher = RottenTomatoesSearcher()
+
+    @patch("rotten_tomatoes_client.RottenTomatoesClient.search")
+    def test_search(self, mocked_search):
+        term = "term"
+        tv_show_results = "tv show results"
+        movie_results = "movie results"
+        results = {"movies": movie_results, "tvSeries": tv_show_results}
+        movies = "movies"
+        tv_shows = "tv shows"
+        self.searcher.tv_shows_parser.parse = Mock("parse_tv_shows")
+        self.searcher.tv_shows_parser.parse.return_value = tv_shows
+        self.searcher.movies_parser.parse = Mock("parse_movies")
+        self.searcher.movies_parser.parse.return_value = movies
+        mocked_search.return_value = results
+        expected = SearchResult(movies=movies, tv_shows=tv_shows)
+
+        search_result = self.searcher.search(term=term)
+
+        mocked_search.assert_called_once_with(term=term)
+        self.searcher.tv_shows_parser.parse.assert_called_once_with(tv_show_results=tv_show_results)
+        self.searcher.movies_parser.parse.assert_called_once_with(movie_results=movie_results)
+        self.assertEqual(expected, search_result)
