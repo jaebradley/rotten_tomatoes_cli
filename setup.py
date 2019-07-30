c93b8346a202a557ac8d53e1015afb12593c53e8
Jae Bradley <jae.b.bradley@gmail.com>
2019-07-29 20:42:01 -0700

    Update setup.py and fix security vulnerabilities in requirements.txt
    Resolves existing security vulnerabilities involving the `pyyaml`, `urllib3`, and `requests` dependencies.
    
    ![image](https://user-images.githubusercontent.com/8136030/62098883-1ff0bd80-b241-11e9-9210-40a93ab648db.png)

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="rotten_tomatoes_cli",
     description="Rotten Tomatoes Command Line Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jae Bradley",
     author_email="jae.b.bradley@gmail.com",
     url="https://github.com/jaebradley/rotten_tomatoes_cli",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
+    python_requires=">=3.4",
     install_requires=[
         "Click==6.7",
         "rotten_tomatoes_client==0.0.3",
@@ -20,6 +26,9 @@ setup(
             "rotten= scripts.rotten:rotten"
         ],
     },
-    keywords=["rotten_tomatoes"],
+    keywords=[
+        "Movies",
+        "Rotten Tomatoes",
+    ],
     classifiers=[]
 )
