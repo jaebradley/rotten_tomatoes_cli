9cecf3d7028aa238f1fb07e53b702e389d94f973
Jae Bradley <jae.b.bradley@gmail.com>
2019-07-30 07:55:48 -0700

    Remove README.rst and setup.cfg (#18)
    Resolves issue where `README.md` is not found when installing `0.0.2`.
    
    https://github.com/jaebradley/rotten_tomatoes_cli/pull/16#issuecomment-516317770

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Jae Bradley",
     author_email="jae.b.bradley@gmail.com",
     url="https://github.com/jaebradley/rotten_tomatoes_cli",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     python_requires=">=3.4",
